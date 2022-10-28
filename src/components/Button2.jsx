b423630a6de312223297353361b9b2ebc87fa875
jeonghs2015 <hsjung201558@gmail.com>
2022-10-28 15:28:22 +0900

    css 총 정리 코드

--- src/components/Button2.jsx
+++ src/components/Button2.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import styles from './Button2.module.css';
 
 export default function Button2() {
-  return <button className={styles.button}>Button 2</button>;
+  return <button className={styles.button}>Button2</button>;
 }
